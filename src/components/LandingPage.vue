953d1f5678c62dda2b1a40144b359083d2fda696
raidas <mrraidas@gmail.com>
2023-09-18 20:21:56 +0300

    update landing page

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -2,34 +2,34 @@
 import { ref, onMounted } from 'vue'
 import { useThemeVars } from 'naive-ui'
 
-
-// TODO:
-//  1. Scale on hover
-//  2. Auto hover on mobile and loop over the items 
 const themeVars = useThemeVars()
 
+// emoji configs
+const emoji = ref('')
+const blurEmoji = ref(false)
+
+// Id-Mask char loop
 const word = ref('Mask')
+const words = ['Mask']
 const alphabet = '0123456789abcdefghijklmnopqrstuvwxyzM '
-const words = [
-  'Mask',
-]
 
 const sleep = (ms) => {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-const replaceOrAddCharacter = (str, idx, repl) => {
-  // add
-  if (idx > str.length) {
-    return str + repl
-  }
-  // replace
-  let start = str.substring(0, idx)
-  let end = str.substring(idx + 1, str.length)
-  return start + repl + end
-}
-
 const loopOverWords = async () => {
+
+  const replaceOrAddCharacter = (str, idx, repl) => {
+    // add
+    if (idx > str.length) {
+      return str + repl
+    }
+    // replace
+    let start = str.substring(0, idx)
+    let end = str.substring(idx + 1, str.length)
+    return start + repl + end
+  }
+
   while (true) {
 
     for (let next_word_idx = 0; next_word_idx < words.length; next_word_idx++) {
@@ -61,8 +61,33 @@ const loopOverWords = async () => {
   }
 }
 
+const loopOverEmojis = async () => {
+
+  const emojis = [
+    "👩‍⚕️", "👩‍🎓", "👩‍🏫", "👩‍⚖️", "👩‍🌾", "👩‍🍳", "👩‍🔧", "👩‍🏭", "👩‍💼", "👩‍🔬", "👩‍💻",
+    "👩‍🎤", "👩‍🎨", "🧑‍🚀", "🧑‍🚒", "👮‍♂️", "🕵️‍♂️", "💂‍♂️", "👷", "🫅", "🤵", "👰",
+    "🎅", "🦸", "🦹", "🧙", "🧚", "🧛", "🧜", "🧝", "🧞", "🧟"
+  ]
+  while (true) {
+    const randomIndex = Math.floor(Math.random() * emojis.length)
+    emoji.value = emojis[randomIndex]
+    await sleep(4000)
+  }
+}
+
+const applyRandomEmojiBlur = async () => {
+  while (true) {
+    blurEmoji.value = false
+    await sleep(5000)
+    blurEmoji.value = true
+    await sleep(2000)
+  }
+}
+
 onMounted( async () => {
   loopOverWords()
+  loopOverEmojis()
+  applyRandomEmojiBlur()
 })
 </script>
 
@@ -71,10 +96,10 @@ onMounted( async () => {
   <div class="center-container">
     <div class="shine1" style="width: 90em;">
       <n-space vertical align="center">
-        <n-h1 class="shine2 shine3">🦸</n-h1>
-        <n-h1 class="shine2 shine3">id-{{ word }}</n-h1>
-        <n-text depth="3" class="shine2 shine3">
-          zk-powered-<n-gradient-text type="primary">identity</n-gradient-text>
+        <n-text style="font-size: 300%" class="shine2 shine3 transparent" :class="[blurEmoji ? 'blur' : '']">{{ emoji }}</n-text>
+        <n-text style="font-size: 300%" class="shine2 shine3 transparent" id="text1">Id-{{ word }}</n-text>
+        <n-text style="font-size: 130%" depth="3" class="shine2 shine3 transparent">
+          zk-powered-<n-gradient-text type="primary" class="transparent">identity</n-gradient-text>
         </n-text>
       </n-space>
     </div>
@@ -91,11 +116,18 @@ onMounted( async () => {
     height: 100vh;
 }
 
+.blur {
+  filter: blur(4px);
+  transition: color 1s ease, text-shadow 1s ease;
+}
+
+.transparent::selection {
+  background-color: rgba(255,255,255,0.1);
+}
+
 .shine1 {
   will-change: filter;
   transition: filter 600ms;
-}
-.shine1:hover {
   filter: drop-shadow(0 0 2em v-bind(themeVars.primaryColor));
 }
 
