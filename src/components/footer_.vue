c7e8d30b381d2acd2b14d9d118d26a148f39468f
raidas <mrraidas@gmail.com>
2023-09-22 19:15:00 +0300

    init footer

---
+++ src/components/footer_.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useThemeVars } from 'naive-ui'
+
+const themeVars = useThemeVars()
+
+onMounted( async () => {
+  console.log('footer mounted')
+})
+
+
+</script>
+
+<template>
+
+  <div class="centered-text">
+  </div>
+</template>
+
+<style scoped>
+</style>
