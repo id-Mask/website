88fe4c022e3f61253ce648418346522aad393be0
raidas <mrraidas@gmail.com>
2023-10-13 11:36:30 +0300

    temp component

---
+++ src/components/hljsTest.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref } from 'vue'
+import { useThemeVars } from 'naive-ui'
+
+// hljs stuff
+import hljs from 'highlight.js/lib/core'
+import json from 'highlight.js/lib/languages/json'
+hljs.registerLanguage('json', json)
+
+const themeVars = useThemeVars()
+
+const jsonData = ref({
+  numberKey: 123456,
+  stringKey: 'askdjhasd'
+})
+
+</script>
+
+<template>
+  <n-code :code="JSON.stringify(jsonData, null, 2)" :hljs="hljs" language="json" class="code" style="white-space: nowrap;"/>
+</template>
+
+<style scoped>
+.code {
+  font-family: "JetBrains Mono";
+  font-size: 90%;
+}
+
+.hljs-punctuation {
+  color: v-bind(themeVars.textColor3) !important;
+}
+
+.hljs-attr {
+  color: v-bind(themeVars.textColor3) !important;
+}
+
+.hljs-number {
+  color: v-bind(themeVars.primaryColor) !important;
+}
+
+.hljs-string {
+  color: v-bind(themeVars.primaryColor) !important;
+}
+</style>
