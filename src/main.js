a0071822ad74ac6f3b3d5d2dd1dd66e5c5a10bc5
raidas <mrraidas@gmail.com>
2023-10-20 14:07:48 +0300

    add vercel analytics

--- src/main.js
+++ src/main.js
@@ -9,6 +9,10 @@ import '@fontsource/jetbrains-mono'
 import naive from 'naive-ui'
 import store from './store/index'
 
+// vercel analytics
+import { inject } from '@vercel/analytics'
+inject()
+
 const app = createApp(App)
 app.use(naive)
 app.use(store)
