d04ebca55604d16e1b5a0fa81f367cb13d486959
raidas <mrraidas@gmail.com>
2024-01-20 18:35:34 +0200

    init google wallet

--- src/components/proofSteps/saveProofToGoogleWallet.vue
+++ src/components/proofSteps/saveProofToGoogleWallet.vue
@@ -77,26 +77,37 @@ const save = async () => {
   let msgReactive = message.create('1/3 Preparing proof 🛠️', { type: 'loading', duration: 10e9 })
 
   const proofJson = store.state.proofs.data[props.selectedProof].proof
-  console.log('proofJson:', proofJson)
+  // console.log('proofJson:', proofJson)
 
   const secret = generateSecret(32)
-  console.log('secret:', secret)
+  // console.log('secret:', secret)
 
   const encryptedProof = encrypt(proofJson, secret)
-  console.log('encryptedProof:', encryptedProof)
+  // console.log('encryptedProof:', encryptedProof)
 
   msgReactive.content = "2/3 Uploading enrypted proof to IPFS ⬆️"
   const ipfsData = await uploadToIPFS(encryptedProof)
-  console.log('ipfsData:', ipfsData)
+  // console.log('ipfsData:', ipfsData)
 
   msgReactive.content = "3/3 Saving proof to your Google wallet ⬆️"
-  // TODO: call the zkOracle  and push this to user's google wallet
+
   const walletData = {
     ipfs: ipfsData,
     secretKey: secret,
     proof: props.selectedProof,
   }
-  console.log('walletData', walletData)
+  // console.log('walletData', walletData)
+
+  const url = store.state.settings.zkOracle + 'createGoogeWalletPass'
+  const identifier = generateSecret(12).replace(/[^a-zA-Z0-9]/g, '')
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify({identifier: identifier, data: walletData})
+  })
+  const response_ = await response.json()
+  const googleWalletUrl = `https://pay.google.com/gp/v/save/${response_.token}`
+  window.open(googleWalletUrl, '_blank')
 
   msgReactive.type = 'success'
   msgReactive.content = "You can now access your proof from within your Google wallet 👛"
@@ -109,14 +120,11 @@ const save = async () => {
 </script>
 
 <template>
-  <n-button @click="save()" :loading="isLoading" type="primary">
-    <template #icon>
-      <n-icon :color="themeVars.textColor">
-        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512"><path d="M95.5 104h320a87.73 87.73 0 0 1 11.18.71a66 66 0 0 0-77.51-55.56L86 94.08h-.3a66 66 0 0 0-41.07 26.13A87.57 87.57 0 0 1 95.5 104z" fill="currentColor"></path><path d="M415.5 128h-320a64.07 64.07 0 0 0-64 64v192a64.07 64.07 0 0 0 64 64h320a64.07 64.07 0 0 0 64-64V192a64.07 64.07 0 0 0-64-64zM368 320a32 32 0 1 1 32-32a32 32 0 0 1-32 32z" fill="currentColor"></path><path d="M32 259.5V160c0-21.67 12-58 53.65-65.87C121 87.5 156 87.5 156 87.5s23 16 4 16s-18.5 24.5 0 24.5s0 23.5 0 23.5L85.5 236z" fill="currentColor"></path></svg>
-      </n-icon>
-    </template>
-    Save to Google wallet
-  </n-button>
+  <n-spin :show="isLoading" size="tiny">
+    <n-thing @click="save()" style="cursor: pointer;">
+      <img width="200" src="../../assets/google-wallet-button.png" />
+    </n-thing>
+  </n-spin>
 </template>
 
 <style>
