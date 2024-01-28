e14ad4c5b62e1b6e50080ae3330ebf05fa3d8ff2
raidas <mrraidas@gmail.com>
2024-01-28 17:56:48 +0200

    update QR scanning

--- src/components/qrcodeScanner.vue
+++ src/components/qrcodeScanner.vue
@@ -2,13 +2,17 @@
 import { ref } from 'vue'
 import * as CryptoJS from 'crypto-js'
 import { QrcodeStream } from 'vue-qrcode-reader'
-import { verify } from 'o1js';
+import { verify } from 'o1js'
 
 import { useStore } from 'vuex'
+import { useThemeVars } from 'naive-ui'
+import { useMessage } from 'naive-ui'
 
 import { proofOfAge } from './zkPrograms/ProofOfAge.js'
 import { proofOfSanctions } from './zkPrograms/ProofOfSanctions.js'
 import { proofOfUniqueHuman } from './zkPrograms/ProofOfUniqueHuman.js'
+
+import { sleep } from './../utils.js'
 
 const proofs = {
   proofOfAge: proofOfAge,
@@ -17,57 +21,60 @@ const proofs = {
 }
 
 const store = useStore()
+const themeVars = useThemeVars()
+const message = useMessage()
 
 const decodedValue = ref(null)
 const proofData = ref(null)
 const isOpen = ref(false)
 
 const isLoading = ref(false)
-const progressText = ref('')
+const progressBarWidth = ref(0)
 
 const verifyProof = async (data) => {
 
   // fetch data from IPFS
+  let msg = message.create('Fetching a proof from IPFS 📡', { type: 'loading', duration: 10e9 })
   const ipfsUrl = 'https://ipfs.io/ipfs/'
   const response = await fetch(ipfsUrl + data.ipfs.IpfsHash)
   const response_ = await response.json()
-  progressText.value = 'fetched data from IPFS'
   
   // decrypt data
+  msg.content = "Decrypting data 🔐"
   var bytes  = CryptoJS.AES.decrypt(response_, data.secretKey)
   var proof = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
-  progressText.value = 'decripted'
 
   // check if the proof is compiled and vk saved
   // if not compile and save verificationKey to store
+  msg.content = "Compiling zk program 🛠️"
   if (!store.state.proofs.data[data.proof].verificationKey) {
-    progressText.value = 'compiling'
     const { verificationKey } = await proofs[data.proof].compile()
     store.state.proofs.data[data.proof].verificationKey = verificationKey
   }
 
   // verify if the provided proof is correct
+  msg.content = "Verifying the proof 🧐"
   try {
-    progressText.value = 'verifying'
     let ok = await verify(proof, store.state.proofs.data[data.proof].verificationKey)
     if (ok) {
-      progressText.value = 'verified!'
+      msg.type = 'success'
+      msg.content = "The proof is valid!"
       proofData.value = proof.publicOutput
     } else {
-      progressText.value = 'Somethings wrong'
+      console.log('error')
     }
   } catch (error) {
-    progressText.value = 'Somethings wrong 2'
+    console.error(error)
   } finally {
     isLoading.value = false
-    decodedValue.value = null
+    await sleep(4000)
+    msg.destroy()
   }
 }
 
 const onDetect = async (value) => {
   console.log('DETECTED')
   isLoading.value = true
-  progressText.value = 'detected'
   decodedValue.value = value.map((code) => JSON.parse(code.rawValue))
   await verifyProof(decodedValue.value[0])
 };
@@ -76,7 +83,8 @@ const paintOutline = (detectedCodes, ctx) => {
   for (const detectedCode of detectedCodes) {
     const [firstPoint, ...otherPoints] = detectedCode.cornerPoints
 
-    ctx.strokeStyle = 'red'
+    ctx.strokeStyle = themeVars.value.primaryColor
+    ctx.lineWidth = 20
 
     ctx.beginPath()
     ctx.moveTo(firstPoint.x, firstPoint.y)
@@ -92,27 +100,38 @@ const paintOutline = (detectedCodes, ctx) => {
 </script>
 
 <template>
-  <n-button type="primary" @click="isOpen = !isOpen">scan QR code
-  </n-button>
+  <n-button type="primary" @click="isOpen = !isOpen">scan QR code</n-button>
   <div v-if="isOpen">
     <n-modal style="width: 90%" :mask-closable="true" v-model:show="isOpen">
       <n-card>
-        <qrcode-stream @detect="onDetect" :track="paintOutline" @errors="console.error" />
+        <n-spin :show="isLoading">
+          <qrcode-stream @detect="onDetect" :track="paintOutline" @errors="console.error" />
+        </n-spin>
         <template #footer>
           <n-space align="center" vertical>
-            <div v-if="decodedValue">
-              {{ decodedValue }}
-            </div>
-            <n-text type="success" tag="h2" v-else>
-              Scan QR code 🧐
-            </n-text>
-            <n-text type="success" tag="h3">
-              <n-spin size="small" v-if="isLoading" />
-              {{ progressText }}
-            </n-text>
-            <div>
-              {{ proofData }}
-            </div>
+            <n-card v-if="decodedValue && proofData" size="large" :hoverable="true"> 
+              <n-text type="success" tag="h4">
+
+                <div v-if="decodedValue[0].proof == 'proofOfAge'">
+                  Older than: {{ proofData[0] }}
+                  <br/>
+                  Created at: {{ proofData[1] }}
+                </div>
+
+                <div v-if="decodedValue[0].proof == 'proofOfUniqueHuman'">
+                  Unique Identifier: {{ proofData[0] }}
+                  <br/>
+                  Created at: {{ proofData[1] }}
+                </div>
+
+                <div v-if="decodedValue[0].proof == 'proofOfNonSanctions'">
+                  OFAC reliability score: {{ proofData[0] }}
+                  <br/>
+                  Created at: {{ proofData[1] }}
+                </div>
+
+              </n-text>
+            </n-card>
           </n-space>
         </template>
       </n-card>
