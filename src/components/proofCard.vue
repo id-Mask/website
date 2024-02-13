747f9f3d1d2ea1dd28ace5bfcef30dbcfda63e40
raidas <mrraidas@gmail.com>
2024-02-13 18:48:31 +0200

    update arrow placement

--- src/components/proofCard.vue
+++ src/components/proofCard.vue
@@ -142,7 +142,25 @@ const triggerNextStep_ = () => {
             </n-icon-wrapper>
             </n-collapse-transition>
             <n-spin :size="15" v-if="isLoading" style="padding-top: 4px;"/>
-            <span v-else>{{ currentStep + 1 }}</span> / {{ proofs[props.selectedProof].steps.length }}
+            <span v-else>
+              <n-button quaternary size="small">
+                <!-- <template #icon>
+                  <n-icon>
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 256 512"><path d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z" fill="currentColor"></path></svg>
+                  </n-icon>
+                </template> -->
+                <n-text style="font-size: 120%;">{{ currentStep + 1 }}</n-text>
+              </n-button>
+            </span> 
+            / 
+            <n-button quaternary size="small" icon-placement="right">
+              <!-- <template #icon>
+                <n-icon>
+                  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 256 512"><path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4l-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z" fill="currentColor"></path></svg>
+                </n-icon>
+              </template> -->
+              <n-text style="font-size: 120%;">{{ proofs[props.selectedProof].steps.length }}</n-text>
+            </n-button>
           </n-space>
         </n-space>
       <n-progress
