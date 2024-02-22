4673a4e1fd9c8221da21c0723362ac4699e9c3ef
raidas <mrraidas@gmail.com>
2024-02-22 21:34:54 +0200

    fix proof switch logic and other super-small changes

--- src/components/proofCard.vue
+++ src/components/proofCard.vue
@@ -1,13 +1,15 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { useStore } from 'vuex'
 import getPersonalData from './proofSteps/getPersonalData.vue'
 import createProofOfAge from './proofSteps/createProofOfAge.vue'
 import createProofOfSanctions from './proofSteps/createProofOfSanctions.vue'
 import createProofOfUniqueHuman from './proofSteps/createProofOfUniqueHuman.vue'
 import saveProof from './proofSteps/saveProof.vue'
+import { useIsMobile } from '../utils.js'
 
 const store = useStore()
+const isMobile = useIsMobile()
 const props = defineProps({
   selectedProof: String,
 })
@@ -65,6 +67,13 @@ const setFinished = (val) => {
   // in practive this will only be the first stop, I guess..?
   // This might have to be refactored later, not a perfect design.
   const stepName = proofs.value[props.selectedProof].steps[getCurrentStep()].component
+
+  // abort if step name not getPersonalData
+  // we don't want to set finished to for example save proof step
+  if (stepName != 'getPersonalData') {
+    return
+  }
+
   for (let proof in proofs.value) {
     for (let step = 0; step < proofs.value[proof].steps.length; step++) {
       if (proofs.value[proof].steps[step].component == stepName) {
@@ -79,14 +88,7 @@ const setLoading = (val) => {
 }
 
 const getCurrentStep = () => {
-  // whenever we switch from one proof to another, it might be the case that
-  // previously selected proof had more steps that newly selected proof
-  // thats why in such case we must move to maxStepLength instad of currentStep
   const maxStepLength = proofs.value[props.selectedProof].steps.length - 1
-  
-  // const finishedIndex = proofs.value[props.selectedProof].steps.findIndex(item => item.finished === true) + 1
-  // console.log(currentStep.value, finishedIndex)
-
   return currentStep.value > maxStepLength ? maxStepLength : currentStep.value
 }
 
@@ -95,6 +97,23 @@ const triggerNextStep_ = () => {
     currentStep.value ++
   }
 }
+
+watch(() => props.selectedProof, () => {
+  // whanever the user picks another proof
+  // go the the last consecutive finished step
+  // TODO: this is overly complicated. must refactor this at some point.
+  let lastIndex = 0;
+  for (let i = 0; i <= proofs.value[props.selectedProof].steps.length - 1; i++) {
+    if (proofs.value[props.selectedProof].steps[i].finished === true) {
+      lastIndex += 1;
+    } else if (lastIndex !== 0) {
+      break;
+    }
+  }
+
+  // make sure do not select over the last step, i.e. max is always len of steps
+  currentStep.value = Math.min(lastIndex, proofs.value[props.selectedProof].steps.length - 1)
+})
 
 </script>
 
@@ -106,15 +125,15 @@ const triggerNextStep_ = () => {
   >
     <template #header>
       <n-space vertical :size="20">
-        <n-space justify="space-between">
+        <n-space :justify="isMobile ? 'center' : 'space-between'">
           <div>Create {{ mapping[props.selectedProof] }}</div>
           <n-space horizontal align="start">
             <n-collapse-transition :show="proofs[props.selectedProof].steps[getCurrentStep()].finished">
-            <n-icon-wrapper>
-              <n-icon :size="18">
-                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24"><path d="M9 16.2l-3.5-3.5a.984.984 0 0 0-1.4 0a.984.984 0 0 0 0 1.4l4.19 4.19c.39.39 1.02.39 1.41 0L20.3 7.7a.984.984 0 0 0 0-1.4a.984.984 0 0 0-1.4 0L9 16.2z" fill="currentColor"></path></svg>
-              </n-icon>
-            </n-icon-wrapper>
+              <n-icon-wrapper>
+                <n-icon :size="14">
+                  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24"><path d="M9 16.2l-3.5-3.5a.984.984 0 0 0-1.4 0a.984.984 0 0 0 0 1.4l4.19 4.19c.39.39 1.02.39 1.41 0L20.3 7.7a.984.984 0 0 0 0-1.4a.984.984 0 0 0-1.4 0L9 16.2z" fill="currentColor"></path></svg>
+                </n-icon>
+              </n-icon-wrapper>
             </n-collapse-transition>
             <n-spin :size="15" v-if="isLoading" style="padding-top: 4px;"/>
             <span v-else>
@@ -129,7 +148,7 @@ const triggerNextStep_ = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 16 16"><g fill="none"><path d="M10.354 3.146a.5.5 0 0 1 0 .708L6.207 8l4.147 4.146a.5.5 0 0 1-.708.708l-4.5-4.5a.5.5 0 0 1 0-.708l4.5-4.5a.5.5 0 0 1 .708 0z" fill="currentColor"></path></g></svg>
                   </n-icon>
                 </template>
-                <n-text style="font-size: 120%;">{{ currentStep + 1 }}</n-text>
+                <n-text style="font-size: 124%;">{{ currentStep + 1 }}</n-text>
               </n-button>
             </span> 
             <n-text style="font-size: 95%">/</n-text>
