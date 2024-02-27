d1f1a2392f32121f825eccb347d37788f70e4b61
raidas <mrraidas@gmail.com>
2024-02-27 22:58:00 +0200

    add modifyable endpoints (block explorer, graph ql, zk-oracle)

--- src/components/header_.vue
+++ src/components/header_.vue
@@ -82,6 +82,62 @@ onMounted(() => {
               </n-icon>
             </template>
           </n-button>
+
+          <n-popover
+            trigger="click"
+            :show-arrow="false"
+          >
+            <template #trigger>
+              <n-button tertiary type="primary" size="large">
+                <template #icon>
+                  <n-icon :color="themeVars.primaryColor">
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066-2.573c-.94-1.543.826-3.31 2.37-2.37c1 .608 2.296.07 2.572-1.065z"></path><circle cx="12" cy="12" r="3"></circle></g></svg>
+                  </n-icon>
+                </template>
+              </n-button>
+            </template>
+            <div>
+              <n-space vertical>
+                <n-h4 type="primary">Enpoint Settings</n-h4>
+                <n-text depth="3" style="font-size: 90%">graphQL</n-text>
+                <n-input-group>
+                  <n-input :value="store.state.settings.graphQLURL" type="text" placeholder="graphQLURL" />
+                  <n-button secondary type="primary">
+                    <template #icon>
+                      <n-icon :color="themeVars.primaryColor">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24"><g fill="none"><path d="M6.75 3h-1A2.75 2.75 0 0 0 3 5.75v12.5A2.75 2.75 0 0 0 5.75 21H6v-6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 15v6h.25A2.75 2.75 0 0 0 21 18.25V8.286a3.25 3.25 0 0 0-.952-2.299l-2.035-2.035A3.25 3.25 0 0 0 15.75 3v4.5a2.25 2.25 0 0 1-2.25 2.25H9A2.25 2.25 0 0 1 6.75 7.5V3zm7.5 0v4.5a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75V3h6zm2.25 18v-6a.75.75 0 0 0-.75-.75h-7.5a.75.75 0 0 0-.75.75v6h9z" fill="currentColor"></path></g></svg>
+                      </n-icon>
+                    </template>
+                  </n-button>
+                </n-input-group>
+
+                <n-text depth="3" style="font-size: 90%">Block explorer</n-text>
+                <n-input-group>
+                  <n-input :value="store.state.settings.blockExplorer" type="text" placeholder="graphQLURL" />
+                  <n-button secondary type="primary">
+                    <template #icon>
+                      <n-icon :color="themeVars.primaryColor">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24"><g fill="none"><path d="M6.75 3h-1A2.75 2.75 0 0 0 3 5.75v12.5A2.75 2.75 0 0 0 5.75 21H6v-6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 15v6h.25A2.75 2.75 0 0 0 21 18.25V8.286a3.25 3.25 0 0 0-.952-2.299l-2.035-2.035A3.25 3.25 0 0 0 15.75 3v4.5a2.25 2.25 0 0 1-2.25 2.25H9A2.25 2.25 0 0 1 6.75 7.5V3zm7.5 0v4.5a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75V3h6zm2.25 18v-6a.75.75 0 0 0-.75-.75h-7.5a.75.75 0 0 0-.75.75v6h9z" fill="currentColor"></path></g></svg>
+                      </n-icon>
+                    </template>
+                  </n-button>
+                </n-input-group>
+
+                <n-text depth="3" style="font-size: 90%">zkOracle</n-text>
+                <n-input-group>
+                  <n-input :value="store.state.settings.zkOracle" type="text" placeholder="graphQLURL" />
+                  <n-button secondary type="primary">
+                    <template #icon>
+                      <n-icon :color="themeVars.primaryColor">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24"><g fill="none"><path d="M6.75 3h-1A2.75 2.75 0 0 0 3 5.75v12.5A2.75 2.75 0 0 0 5.75 21H6v-6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 15v6h.25A2.75 2.75 0 0 0 21 18.25V8.286a3.25 3.25 0 0 0-.952-2.299l-2.035-2.035A3.25 3.25 0 0 0 15.75 3v4.5a2.25 2.25 0 0 1-2.25 2.25H9A2.25 2.25 0 0 1 6.75 7.5V3zm7.5 0v4.5a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75V3h6zm2.25 18v-6a.75.75 0 0 0-.75-.75h-7.5a.75.75 0 0 0-.75.75v6h9z" fill="currentColor"></path></g></svg>
+                      </n-icon>
+                    </template>
+                  </n-button>
+                </n-input-group>
+              </n-space>
+            </div>
+          </n-popover>
+
         </n-space>
       </template>
       <template #footer>
