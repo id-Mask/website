c28ef89d737c420e3a0d535c01de605f8033ffa5
raidas <mrraidas@gmail.com>
2024-04-22 15:20:36 +0300

    add QR code scanner to the app (commit 5)

--- src/components/header_.vue
+++ src/components/header_.vue
@@ -5,12 +5,15 @@ import { useIsMobile } from '../utils'
 import { useStore } from 'vuex'
 import { sleep } from './../utils.js'
 
+import { QrcodeStream } from 'vue-qrcode-reader'
+
 const themeVars = useThemeVars()
 const isMobile = useIsMobile()
 const store = useStore()
 const emits = defineEmits(['logoClick'])
 
 const char = ref('-')
+const showScannerModal = ref(false)
 
 const loopChars = async () => {
   const charSet = ['—', '\\', '|', '/', '—', '\\', '|', '/', '-']
@@ -22,6 +25,14 @@ const loopChars = async () => {
       index++
       await sleep(100)
     }
+  }
+}
+
+const onQrCodeDetect = (detectedCodes) => {
+  if (detectedCodes.length > 0) {
+    const url = detectedCodes[0].rawValue
+    window.open(url, '_blank')
+    // window.open('https://www.google.com/', '_blank')
   }
 }
 
@@ -79,6 +90,14 @@ onMounted(() => {
                 <div v-else>
                   <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24"><path d="M12 3h.393a7.5 7.5 0 0 0 7.92 12.446A9 9 0 1 1 12 2.992z" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
                 </div>
+              </n-icon>
+            </template>
+          </n-button>
+
+          <n-button tertiary type="primary" size="large" @click="showScannerModal = !showScannerModal">
+            <template #icon>
+              <n-icon :color="themeVars.primaryColor">
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve"><g><path d="M96,124.2c0-6.9,5.2-12.2,12.2-12.2H176V64h-66.8C75.7,64,48,90.7,48,124.2V192h48V124.2z"></path><path d="M403.6,64H336v48h67.2c6.9,0,12.8,5.2,12.8,12.2V192h48v-67.8C464,90.7,437,64,403.6,64z"></path><path d="M416,386.8c0,6.9-5.2,12.2-12.2,12.2H336v49h67.8c33.5,0,60.2-27.7,60.2-61.2V320h-48V386.8z"></path><path d="M108.2,399c-6.9,0-12.2-5.2-12.2-12.2V320H48v66.8c0,33.5,27.7,61.2,61.2,61.2H176v-49H108.2z"></path></g></svg>
               </n-icon>
             </template>
           </n-button>
@@ -177,6 +196,17 @@ onMounted(() => {
       <template #footer>
       </template>
     </n-page-header>
+
+      <n-modal v-model:show="showScannerModal">
+        <n-card style="max-width: 50em;">
+          <qrcode-stream 
+            @detect="onQrCodeDetect" 
+            @errors="console.error" 
+            style="max-width: 50em;"
+          />
+        </n-card>
+      </n-modal>
+
   </n-layout-header>
 </template>
 
