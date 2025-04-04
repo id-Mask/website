8d0db2950c84a3a79fa6a44e3eb3dce7dbbdaed8
raidas <mrraidas@gmail.com>
2025-04-04 17:43:28 +0300

    remove qr scanner

--- src/components/header_.vue
+++ src/components/header_.vue
@@ -94,13 +94,13 @@ onMounted(() => {
             </template>
           </n-button>
 
-          <n-button tertiary type="primary" size="large" @click="showScannerModal = !showScannerModal">
+          <!-- <n-button tertiary type="primary" size="large" @click="showScannerModal = !showScannerModal">
             <template #icon>
               <n-icon :color="themeVars.primaryColor">
                 <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve"><g><path d="M96,124.2c0-6.9,5.2-12.2,12.2-12.2H176V64h-66.8C75.7,64,48,90.7,48,124.2V192h48V124.2z"></path><path d="M403.6,64H336v48h67.2c6.9,0,12.8,5.2,12.8,12.2V192h48v-67.8C464,90.7,437,64,403.6,64z"></path><path d="M416,386.8c0,6.9-5.2,12.2-12.2,12.2H336v49h67.8c33.5,0,60.2-27.7,60.2-61.2V320h-48V386.8z"></path><path d="M108.2,399c-6.9,0-12.2-5.2-12.2-12.2V320H48v66.8c0,33.5,27.7,61.2,61.2,61.2H176v-49H108.2z"></path></g></svg>
               </n-icon>
             </template>
-          </n-button>
+          </n-button> -->
 
           <n-popover
             trigger="click"
